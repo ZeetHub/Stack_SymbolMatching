79e24bd87189c1780a1aefe25b836f9bfe1dce3a
Zeet <zeet09@gmail.com>
2020-11-05 12:29:37 +0300

    stack ADS Array Implementation Modules

--- CheckBalance.cpp
+++ CheckBalance.cpp
@@ -1,4 +1,10 @@
 #include<iostream>
+/*
+        Caller-->Callee
+by value: N --> M or *N --> M
+by alias: N --> &M or N --> *&M
+by pointer: N --> *M or *&N --> *M
+*/
 
 using namespace std;
 
@@ -37,7 +43,7 @@ bool isEmpty(stack<T> st)
 template<class T>
 bool push(stack<T> *st, T item)
 {
-    if (!isFull(st))
+    if (!isFull(*st))
         {
             st->baseArray[++st->top] = item;
             st->count++;
@@ -48,11 +54,11 @@ bool push(stack<T> *st, T item)
 }
 
 template<class T>
-bool pop(stack<T> *st, T& takePoppedItem = NIL)
+bool pop(stack<T> *st/*, T& takePoppedItem = NIL*/)
 {
-    if(!isEmpty(st))
+    if(!isEmpty(*st))
     {
-        takePoppedItem = st->baseArray[st->top];
+        // takePoppedItem = st->baseArray[st->top];
         st->top--;
         return true;
     }
@@ -63,7 +69,7 @@ bool pop(stack<T> *st, T& takePoppedItem = NIL)
 template<class T>
 T peekTop(stack<T> st)
 {
-    return st->baseArray[st->top];
+    return st.baseArray[st.top];
 }
 
 template<class T>
@@ -71,20 +77,28 @@ bool resize(stack<T>* st, int capacity)
 {
     T* temp;
     int tempCapacity = st->capacity;
-    temp = new(nothrow)T[st->capacity];//may throw NULL, but rare.
-    for(int i=0;i<st->capacity;i++)
+    temp = new(nothrow)T[st->capacity];
+    if (!temp)
+    {
+        for(int i=0;i<st->capacity;i++)
         temp[i] = st->baseArray[i];
     
-    delete [] st->baseArray;
-    
-    createStack(&st, capacity);
+        delete [] st->baseArray;
+        
+        createStack(*&st, capacity);
 
-    for(int i=0;i<tempCapacity;i++)
-    {
-        st->baseArray[i] = temp[i];
-        count++;
-        st->top++;
+        for(int i=0;i<tempCapacity;i++)
+        {
+            st->baseArray[i] = temp[i];
+            st->count++;
+            st->top++;
+        }
+
+        return true;
     }
+    
+    return false;
+
 }
 
 template<class T>
@@ -98,11 +112,38 @@ void destroy(stack<T> *st)
 {
     clear(st);
     delete [] st->baseArray;
-
 }
 
+template<class T>
+void displayStack(stack<T> st)
+{
+    for(int i=st.top; i>=0;i--)
+        cout<< st.baseArray[i]<<" ";
+}
 
 int main()
 {
+    stack<int> st;
+    createStack(&st, 5);
+    push(&st, 45);
+	push(&st, 35);
+	push(&st, 25);
+	push(&st, 15);
+	push(&st, 5);
+
+    displayStack(st);
+
+    cout<<endl<<peekTop(st)<<endl;
+
+    resize(&st, 10);
+
+    pop(&st);
+    pop(&st);
+
+    cout<<peekTop(st)<<endl;
+
+    displayStack(st);
+    destroy(&st);
+    displayStack(st);
 
 }
